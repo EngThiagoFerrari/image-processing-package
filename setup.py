0444bf4e32ce89b8571f5123b132baff32720890
Thiago Ferrari <engthiagoferrari@gmail.com>
2024-09-12 00:56:13 -0300

    First commit of the processing image package - my solution to dio.me bootcamp NTT DATA

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("READMEL.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name= "image_processing",
+    version= "0.0.1",
+    author= "Thiago Ferrari",
+    author_email= "engthiagoferrari@gmail.com",
+    description= "modules for transforming images and its colors through a histogram reference", 
+    long_description= page_description,
+    long_description_content_type= "text/markdown",
+    url= " ", #inserir meu github
+    packages= find_packages(),
+    install_requires= requirements,
+    python_requires= '>=3.10',
+)
